2b27b4eda8981d2a129c311da496292751e866bb
Angel Feliz <angel.esteban.feliz@gmail.com>
2024-06-20 00:30:08 -0400

    uptating R version and adding duckdb as a dependency

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rocker/rstudio:4.4.0
+FROM rocker/rstudio:4.4.1
 
 # Install jq to parse json files
 RUN apt-get update && apt-get install -y --no-install-recommends \
--- run-docker.sh
+++ run-docker.sh
@@ -1,14 +1,12 @@
 # run the container with the host cache mounted in the container
 docker run -d --rm -ti \
     --name TaxiProject \
-    -e PASSWORD=wxz \
-    -e ROOT=true  \
     -e DISABLE_AUTH=true \
     -p 127.0.0.1:5555:8787 \
-    -v /home/angel/R-Folder:/home/rstudio \
-    -v /home/angel/R-Folder/r-lib-4.4:/usr/local/lib/R/site-library \
-    -e "RENV_PATHS_CACHE=/home/rstudio/.cache/R/renv/cache/v5/R-4.3/x86_64-pc-linux-gnu" \
-    -v /home/angel/R-Folder/cache_4.3:/home/rstudio/.cache/R/renv/cache/v5/R-4.3/x86_64-pc-linux-gnu \
+    -v /home/angelfeliz/Documents/r-projects:/home/rstudio \
+    -v /home/angelfeliz/Documents/r-projects/r-lib-4.4:/usr/local/lib/R/site-library \
+    -e "RENV_PATHS_CACHE=/home/rstudio/.cache/R/renv/library/project.nyc.taxi-dd1eca8a/linux-ubuntu-jammy/R-4.4/x86_64-pc-linux-gnu" \
+    -v /home/angelfeliz/Documents/r-projects/cache_4.4:/home/rstudio/.cache/R/renv/library/project.nyc.taxi-dd1eca8a/linux-ubuntu-jammy/R-4.4/x86_64-pc-linux-gnu \
     angelfelizr/project.nyc.taxi:0.0.0.9000
 
 # We need to wait 1 second
